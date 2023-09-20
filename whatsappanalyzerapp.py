e868247dd1130150c595a4f13b2c645a490f7bfd
Shiva Chunbuk <127691335+shiva017@users.noreply.github.com>
2023-09-21 01:55:05 +0530

    Update whatsappanalyzerapp.py

--- whatsappanalyzerapp.py
+++ whatsappanalyzerapp.py
@@ -14,12 +14,11 @@ if st.button("Export WhatsApp Chat"):
     st.markdown("[Click here to export WhatsApp chat](https://web.whatsapp.com/)", unsafe_allow_html=True)
     st.info("Please go to WhatsApp and export the chat. After exporting, come back here and upload the chat file.")
 
-# File uploader
-with st.form(key='file_upload_form'):
-    st.write("Upload a chat file (up to 500MB):")
-    uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False, type=["txt"])
-    submit_button = st.form_submit_button(label='Upload and Analyze')
 
+
+
+# File uploader
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
